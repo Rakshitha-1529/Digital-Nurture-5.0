e5f5fa195a03372bd396bfde564c8a67b8449f99
Rakshitha-1529 <rakshithamolabanti@gmail.com>
2026-07-14 20:36:43 +0530

    React-1

---
+++ week-5/1.ReactJS-HOL/myfirstreact/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignitems: "center",
+      height: "100vh",
+      }}>
+      <h1>Welcome to the first session of React!!</h1>
+    </div>
+  );
+}
+
+export default App;
